7d2d9caf8efa9cf42d72e7face8eb6d764cbfe12
Robert Santos <robertdosantos09@gmail.com>
2023-11-01 18:18:08 -0300

    feat: Adicionando as responsividade para a primeira seção do site.

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,25 @@
 const btnMenuHamburger = document.querySelector('.menu-hamburger');
 const headerResponsive = document.querySelector('.header-nav-responsive');
 
+let screen = window.outerWidth;
+
 let activeMenu = 0;
 
 btnMenuHamburger.addEventListener('click', () => {
   if (activeMenu === 1) {
-    headerResponsive.style.width = '0%'
-    headerResponsive.style.left = '100%'
-    activeMenu = 0
+    headerResponsive.style.width = '0%';
+    headerResponsive.style.left = '100%';
+    activeMenu = 0;
   }
   else {
-    headerResponsive.style.width = '45%'
-    headerResponsive.style.left = '55%'
-    activeMenu = 1
+    if (screen > 600) {
+      headerResponsive.style.width = '45%';
+      headerResponsive.style.left = '55%';
+    }
+    else {
+      headerResponsive.style.width = '75%';
+      headerResponsive.style.left = '25%';
+    }
+    activeMenu = 1;
   }
 })
